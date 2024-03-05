0a4b0b39828e88a087fef11669d8b0ae2ccea5d7
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2024-03-05 17:17:40 +0000

    organized layers

---
+++ script.js
@@ -0,0 +1,3 @@
+// let button clicked = 0 no class
+// if button clicked then button clicked +1 (if btton =1 then you add class) .setatribute
+// if button = 1 then when you click then button clicked -1 (if button = 0 then you remove class).remopveatribute  -->
